689bd76289efb18404013a63653c501bc0711247
sabashubitidze <sabashubitidze210@gmail.com>
2024-06-28 23:55:59 +0400

    homework

--- day 16/homework/lesson1.py
+++ day 16/homework/lesson1.py
@@ -1,9 +1,9 @@
 for i in range(100,501): 
     if i % 3==0:
       print(str(i)+"ეს რიცხვი ყოფს 3 ზე")
-     elif i % 5==:
+    elif  i % 5==:
      print(str(i)+"ეს რიცხვი ყოფს 5 ზე")
-     elif i % 7==0:
+    elif i % 7==0:
      print(str(i)+"ეს რიცხვი ყოფს 7 ზე")
 
 
---
+++ day 20/py.py
@@ -0,0 +1,8 @@
+for i in range(1, 21):
+    if i <= 20:
+        print(i)
+counter = 0
+
+while counter <= 20:
+    print("hello world")
+    counter += 1
