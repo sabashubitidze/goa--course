e8ebcd6bd414e23063e7438768ca81571610bef3
sabashubitidze <sabashubitidze210@gmail.com>
2024-08-14 02:53:08 +0400

    commit

--- day 16/homework/lesson1.py
+++ day 16/homework/lesson1.py
@@ -1,5 +1,4 @@
-for i in range(100,501): 
-    if i % 3==0:
+for i in range(100,501):       if i % 3==0:
       print(str(i)+"ეს რიცხვი ყოფს 3 ზე")
     elif  i % 5==:
      print(str(i)+"ეს რიცხვი ყოფს 5 ზე")
