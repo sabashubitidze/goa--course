16d9215feee9b3012e5aca59557669152655dfba
sabashubitidze <sabashubitidze210@gmail.com>
2024-08-16 13:56:45 +0400

    commit

---
+++ day 28/.py
@@ -0,0 +1,64 @@
+#1
+#for i in range(0,20):
+# print(i)
+
+
+#2
+#nam1=int(input("yuor number"))
+
+#num2=int(input("your namber"))
+
+#for i in range(nam1,num2):
+# print(i)
+
+#3
+#for i in range(50,100):
+ #   print(i)
+
+
+
+#4
+#  #for i in range(100,49,-1)
+# print(i)
+
+#5
+#num1=0
+#for i in renge(0,50):
+#  num1+=i
+
+#6
+#num1=int(input("yor number"))
+#for i in range(0,):
+#  print(i)
+
+#7
+#num1=int(input("your ager"))
+#num2=num1+10
+# for in i renge(num1,num2):
+# print(i)
+
+
+#8
+#num1=int(input("you number"))
+#num2=int(input("you numbers"))
+#print(num1+num2)
+#print(num1-num2)
+#print(num1*num2)
+#print(num1%num2)
+
+#9
+#num1=int(input("your age:"))
+#if num1> 18:
+#    print("youa")
+#else:
+#   print("lefthim in") 
+
+
+#10
+
+#print(302>11)
+#print(2<293)
+#print(33398<=88888)
+#print(2981498>=3273)
+#print(37927!=30924)
+#print(52979==23487)
